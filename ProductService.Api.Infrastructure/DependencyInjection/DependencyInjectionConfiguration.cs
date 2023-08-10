07a5f1772c8ab86619cd4e6266facc591b10404a
Дмитрий Кефирский <kefirskiy@gmail.com>
2023-08-10 11:47:40 +0300

    Add DI Configuration.

---
+++ ProductService.Api.Infrastructure/DependencyInjection/DependencyInjectionConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using ProductService.Api.Repository;
+using ProductService.Application.Services;
+using ProductService.Domain.Contracts.Services;
+
+namespace ProductService.Infrastructure.DependencyInjection;
+
+public static class DependencyInjectionConfiguration
+{
+    public static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IProductsRepository, ProductsRepository>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        services.AddScoped<IProductsService, ProductsService>();
+        
+        return services;
+    }
+} 
