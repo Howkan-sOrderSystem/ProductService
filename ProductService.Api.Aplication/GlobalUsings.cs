fb2d5d29b7965301d1903476041244b0dad04560
Дмитрий Кефирский <kefirskiy@gmail.com>
2023-08-10 11:45:43 +0300

    Add IProductsService implementation.

---
+++ ProductService.Api.Aplication/GlobalUsings.cs
@@ -0,0 +1,3 @@
+﻿global using ProductService.Api.Model;
+global using ProductService.Domain.Contracts.Repositories;
+global using ProductService.Domain.Contracts.Services;
---
+++ ProductService.Api.Aplication/Services/ProductsService.cs
@@ -0,0 +1,33 @@
+﻿namespace ProductService.Application.Services;
+
+public sealed class ProductsService : IProductsService
+{
+    private readonly IProductsRepository _repository;
+
+    public ProductsService(IProductsRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<ProductEntity?> AddAsync(
+        ProductEntity product,
+        CancellationToken cancellationToken = default) =>
+        await _repository.AddAsync(product, cancellationToken);
+
+    public async Task DeleteAsync(Guid id, 
+        CancellationToken cancellationToken = default) => 
+        await _repository.DeleteAsync(id, cancellationToken);
+
+    public async Task<ProductEntity?> GetByIdAsync(Guid id, 
+        CancellationToken cancellationToken = default) => 
+        await _repository.FirstOrDefaultAsync(id, cancellationToken);
+
+    public async Task<IEnumerable<ProductEntity>> GetAllAsync(
+        CancellationToken cancellationToken = default) => 
+        await _repository.GetAllAsync(cancellationToken);
+
+    public async Task<ProductEntity?> UpdateAsync(
+        ProductEntity product,
+        CancellationToken cancellationToken = default) => 
+        await _repository.UpdateAsync(product, cancellationToken);
+}
