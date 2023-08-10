ec355fb6fb25d78d99c63c07bd3b6479c069aa7b
Дмитрий Кефирский <kefirskiy@gmail.com>
2023-08-10 11:46:01 +0300

    Add LoggingConfiguration.

---
+++ ProductService.Api/Configurations/LoggingConfigurations.cs
@@ -0,0 +1,14 @@
+﻿namespace ProductService.Api.Configurations;
+
+public static class LoggingConfigurations
+{
+    public static void AddLoggingConfiguration(
+        this IServiceCollection services, 
+         ConfigureHostBuilder hostBuilder)
+    {
+        hostBuilder.UseSerilog(
+            configureLogger: (context, services, configuration) =>
+            configuration.ReadFrom.Configuration(configuration: context.Configuration)
+            .ReadFrom.Services(services));
+    }
+}
