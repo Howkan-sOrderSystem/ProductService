baeee7cbbb5aa74d97a76e17d3965a017bd8ed56
Дмитрий Кефирский <kefirskiy@gmail.com>
2023-08-10 11:47:25 +0300

    Add Database DI Configuration.

---
+++ ProductService.Api.Infrastructure/DependencyInjection/DatabaseConfiguration.cs
@@ -0,0 +1,14 @@
+﻿namespace ProductService.Infrastructure.DependencyInjection;
+
+public static class DatabaseConfiguration
+{
+    public static void AddDatabaseConfiguration(
+        this IServiceCollection services, 
+        IConfiguration configuration)
+    {
+        services.AddScoped<DbContext, AplicationContext>();
+
+        services.AddDbContext<AplicationContext>(options => options.UseNpgsql(
+            connectionString: configuration.GetConnectionString(name: "DbConnection")));
+    }
+}
