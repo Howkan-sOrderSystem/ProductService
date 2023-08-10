204b7384bb49f3ef4b5e3150827316dabded7224
Дмитрий Кефирский <kefirskiy@gmail.com>
2023-08-10 11:43:27 +0300

    Add Product entity.

---
+++ ProductService.Api.Domain/Entities/ProductEntity.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductService.Api.Model;
+
+public class ProductEntity
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Key]
+    public Guid ProductId { get; set; }
+
+    public string? ProductName { get; set; }
+
+    public string? ProductDescription { get; set; }
+
+    public int? ProductPrice { get; set; }
+}
---
+++ ProductService.Api.Domain/GlobalUsings.cs
@@ -0,0 +1,3 @@
+﻿global using ProductService.Api.Model;
+global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
