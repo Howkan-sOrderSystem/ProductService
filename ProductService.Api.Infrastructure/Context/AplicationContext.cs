04f67ed04656fb2077bb68745abd53b955ab894a
Дмитрий Кефирский <kefirskiy@gmail.com>
2023-08-10 11:46:26 +0300

    Add DbContext.

---
+++ ProductService.Api.Infrastructure/Context/AplicationContext.cs
@@ -0,0 +1,8 @@
+﻿namespace ProductService.Api.Model.Context;
+
+public class AplicationContext : DbContext
+{
+    public DbSet<ProductEntity> Products { get; set; }
+
+    public AplicationContext(DbContextOptions<AplicationContext> options) : base(options) { }
+}
