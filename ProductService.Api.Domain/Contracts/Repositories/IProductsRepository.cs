18c0b853e8f06cc136bbf03cfc2694b81c28caab
Дмитрий Кефирский <kefirskiy@gmail.com>
2023-08-10 11:45:10 +0300

    Add contracts.

---
+++ ProductService.Api.Domain/Contracts/Repositories/IProductsRepository.cs
@@ -0,0 +1,36 @@
+﻿namespace ProductService.Domain.Contracts.Repositories;
+
+public interface IProductsRepository
+{
+    /// <summary>
+    /// Get all products.
+    /// </summary>
+    public Task<IEnumerable<ProductEntity>> GetAllAsync(
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Get product by ID.
+    /// </summary>
+    public Task<ProductEntity?> FirstOrDefaultAsync(Guid id,
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Add new product.
+    /// </summary>
+    public Task<ProductEntity?> AddAsync(
+        ProductEntity product,
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Update product by ID.
+    /// </summary>
+    public Task<ProductEntity?> UpdateAsync(
+        ProductEntity product, 
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Delete product by ID.
+    /// </summary>
+    public Task DeleteAsync(Guid id, 
+        CancellationToken cancellationToken = default);
+}
---
+++ ProductService.Api.Domain/Contracts/Services/IProductsService.cs
@@ -0,0 +1,34 @@
+﻿namespace ProductService.Domain.Contracts.Services;
+
+public interface IProductsService
+{
+    /// <summary>
+    /// Get all products.
+    /// </summary>
+    public Task<IEnumerable<ProductEntity>> GetAllAsync(
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Get Product by ID.
+    /// </summary>
+    public Task<ProductEntity?> GetByIdAsync(Guid id,
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Add new product.
+    /// </summary>
+    public Task<ProductEntity?> AddAsync(ProductEntity product, 
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Update product.
+    /// </summary>
+    public Task<ProductEntity?> UpdateAsync(ProductEntity product, 
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Delete product.
+    /// </summary>
+    public Task DeleteAsync(Guid id,
+        CancellationToken cancellationToken = default);
+}
